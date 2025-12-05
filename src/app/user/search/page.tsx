c73e2a94ede873586ffa246794b8616e01cada0f
tphuong3108 <phuongbeo3108203@gmail.com>
2025-12-05 15:21:34 +0700

    fix search page suspense

--- src/app/user/search/page.tsx
+++ src/app/user/search/page.tsx
@@ -1,7 +1,12 @@
 "use client";
-
 import SearchPageClient from "./SearchPageClient";
 
+import { Suspense } from "react";
+
 export default function Page() {
-  return <SearchPageClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchPageClient />
+    </Suspense>
+  );
 }
